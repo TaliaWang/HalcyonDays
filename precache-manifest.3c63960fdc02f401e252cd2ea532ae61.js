0c6764469f49ce9f8d9290698e19736c912e553d
Adminstrator <adminstrator@Admins-MacBook-Pro.local>
2020-07-11 23:17:43 -0400

    Updates

---
+++ precache-manifest.3c63960fdc02f401e252cd2ea532ae61.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8482e97ad89dba27e557f87068609a01",
+    "url": "/HalcyonDays/index.html"
+  },
+  {
+    "revision": "c7b2f798470990bab85b",
+    "url": "/HalcyonDays/static/css/main.c8f0c697.chunk.css"
+  },
+  {
+    "revision": "2549dd42f779d8a5061f",
+    "url": "/HalcyonDays/static/js/2.725c8aee.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/HalcyonDays/static/js/2.725c8aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b2f798470990bab85b",
+    "url": "/HalcyonDays/static/js/main.959667f9.chunk.js"
+  },
+  {
+    "revision": "c326912c3a42fcb0d5d5",
+    "url": "/HalcyonDays/static/js/runtime-main.eb91dbc0.js"
+  },
+  {
+    "revision": "4bc5e9034228edd5354aea4c6a22cb1d",
+    "url": "/HalcyonDays/static/media/ISOCT2.4bc5e903.ttf"
+  },
+  {
+    "revision": "683719689ce3635687532089a45d510f",
+    "url": "/HalcyonDays/static/media/Nirmala.68371968.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/HalcyonDays/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "55399b73e7afd14e30d2aff817986e51",
+    "url": "/HalcyonDays/static/media/checkmark.55399b73.jpg"
+  }
+]);
